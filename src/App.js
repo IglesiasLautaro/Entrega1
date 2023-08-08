dae4dbffbdc8adca06cb1a5bfb69f3d2a6da02c7
lucaputo <lauiglesias2004@gmail.com>
2023-08-07 21:06:07 -0300

    first commit, creacion proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar></Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;

542bb2818d304cbadbc6c42a2e2682558e6af5ff
lucaputo <lauiglesias2004@gmail.com>
2023-08-10 12:42:01 -0300

    componentes CartWidget e ItemListContainer acomodados

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/NavBar';
-import CartWidget from './components/CartWidget';
+
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
         <Navbar>
-          <CartWidget></CartWidget>
+          
         </Navbar>
-        
+
         <ItemListContainer title="HOLA MUNDO"></ItemListContainer>
     </div>
   );
